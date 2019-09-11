e510e0287a178dfebd7536cca3f6e257e0b82e65
Aleman778 <alexander.mennborg@gmail.com>
2019-09-11 10:25:20 +0200

    Updated a02-parser data structure

--- d7050e/a02_parser/src/main.rs
+++ d7050e/a02_parser/src/main.rs
@@ -25,12 +25,13 @@ extern crate nom;
  * Required nom features for this parser.
  */
 use nom::{
-    bytes::complete::{tag, take_while, take_while1},
+    bytes::complete::{tag, take_while},
     character::complete::{digit1, alpha1, multispace0, multispace1},
-    character::{is_alphabetic, is_alphanumeric},
-    combinator::{map, map_parser, peek},
+    character::{is_alphanumeric},
+    combinator::{map, peek, opt},
     branch::alt,
-    sequence::{pair, tuple, preceded},
+    multi::fold_many0,
+    sequence::{tuple, preceded},
     error,
     Err,
 };
@@ -89,6 +90,47 @@ impl<'a> error::ParseError<Span<'a>> for Error<'a> {
 
 
 /**
+ * Expressions can be a binary operation (two operands),
+ * unary operation (single operand) or a literal.
+ */
+#[derive(Debug, Clone, PartialEq)]
+pub enum Expr<'a> {
+    // Binary Operator
+    BinOp(Box<SpanExpr<'a>>, SpanOp<'a>, Box<SpanExpr<'a>>),
+
+    // Unary operator
+    UnOp(SpanOp<'a>, Box<SpanExpr<'a>>),
+
+    // Local variable declaration
+    Local(Box<SpanExpr<'a>>, SpanType<'a>, Box<SpanExpr<'a>>),
+
+    // Assignment of mutable local variable
+    Assign(Box<SpanExpr<'a>>, Box<SpanExpr<'a>>),
+
+    // Function call
+    Call(Box<SpanExpr<'a>>, Vec<SpanExpr<'a>>),
+
+    // Return expression
+    Return(Box<SpanExpr<'a>>),
+
+    // Identifier
+    Ident(&'a str),
+
+    // Number literal
+    Num(i32),
+
+    // Boolean literal
+    Bool(bool),
+}
+
+
+/**
+ * Type alias of expressions to include span.
+ */
+type SpanExpr<'a> = (Span<'a>, Expr<'a>);
+
+
+/**
  * The different kinds of operators used by
  * both the binary and unary operations.
  */
@@ -118,25 +160,6 @@ type SpanOp<'a> = (Span<'a>, Op);
 
 
 /**
- * Expressions can be a binary operation (two operands),
- * unary operation (single operand) or a literal.
- */
-#[derive(Debug, Clone, PartialEq)]
-pub enum Expr<'a> {
-    BinOp(Box<SpanExpr<'a>>, SpanOp<'a>, Box<SpanExpr<'a>>),
-    UnOp(SpanOp<'a>, Box<SpanExpr<'a>>),
-    Num(i32),
-    Bool(bool),
-}
-
-
-/**
- * Type alias of expressions to include span.
- */
-type SpanExpr<'a> = (Span<'a>, Expr<'a>);
-
-
-/**
  * The different kinds of supported types.
  */
 #[derive(Debug, Clone, PartialEq)]
@@ -153,29 +176,11 @@ type SpanType<'a> = (Span<'a>, Type);
 
 
 /**
- * Identifier struct
+ * Define block as a vector statements.
  */
 #[derive(Debug)]
-pub struct Ident<'a>(&'a str);
+pub struct Block<'a>(Vec<Expr<'a>>);
 
-
-/**
- * Type alias of identifier struct to include span.
- */
-type SpanIdent<'a> = (Span<'a>, Ident<'a>);
-
-
-/**
- * Structure for storing assignments.
- */
-#[derive(Debug)]
-pub struct Assign<'a>(SpanIdent<'a>, SpanType<'a>, Box<SpanExpr<'a>>);
-
-
-/**
- * Type alias of assignment struct to include span.
- */
-type SpanAssign<'a> = (Span<'a>, Assign<'a>);
 
 
 /***************************************************************************
@@ -254,15 +259,18 @@ pub fn parse_literal<'a>(input: Span<'a>) -> IResult<Span<'a>, SpanExpr> {
  */
 fn parse_expr(input: Span) -> IResult<Span, SpanExpr> {
     alt((
-        //Binary operation e.g. 4 + 5
+        // Binary operation e.g. 4 + 5
         map(tuple((parse_literal, preceded(multispace0, parse_binoperator), parse_expr_ms)),
             |(left, op, right)| (input, Expr::BinOp(Box::new(left), op, Box::new(right)))),
         
-        //Unary operation e.g. -5
+        // Unary operation e.g. -5
         map(tuple((parse_unoperator, parse_expr_ms)),
             |(op, right)| (input, Expr::UnOp(op, Box::new(right)))),
 
-        //Literal
+        // Local variable declaration e.g. let a: i32 = 5
+        parse_local,
+        
+        // Literal
         parse_literal,
     ))(input)
 }
@@ -291,19 +299,54 @@ fn parse_type(input: Span) -> IResult<Span, SpanType> {
  * Parses an identifier, has to start with an alphabetic character.
  * e.g. to_string, testVar, hello30.
  */
-fn parse_identifier(input: Span) -> IResult<Span, SpanIdent> {
+fn parse_identifier(input: Span) -> IResult<Span, SpanExpr> {
     peek(alpha1)(input)?;
     map(take_while(|c: char| is_alphanumeric(c as u8)),
-        |s: Span| (s, Ident(s.fragment))
+        |s: Span| (s, Expr::Ident(s.fragment))
     )(input)
 }
 
 
 /**
- * Parse assignments of local variables
+ * Parse a function call. e.g. min(a + 5, 30).
+ */
+fn parse_fn_call(input: Span) -> IResult<Span, SpanExpr> {
+    map(tuple((
+        parse_identifier,
+        preceded(multispace0, tag("(")),
+        opt(preceded(multispace0, parse_expr)),
+        fold_many0(
+            preceded(delimited(multispace0, tag(","), multispace0), parse_expr)
+        ),
+        preceded(multispace0, tag(")")),
+        preceded(multispace0, tag("{")),
+        parse_block,
+        preceded(multispace0, tag("}"))
+    )),
+        |(ident, _, arg0, arg_rest, )|
+}
+
+
+/**
+ * Parse assignment of mutable local variables
+ */
+fn parse_assign(input: Span) -> IResult<Span, SpanExpr> {
+    map(tuple((
+        parse_identifier,
+        preceded(multispace0, tag("=")),
+        preceded(multispace0, parse_expr),
+        preceded(multispace0, tag(";")),
+    )),
+        |(ident, _, expr, _)| (input, Expr::Assign(Box::new(ident), Box::new(expr)))
+    )(input)
+}
+
+
+/**
+ * Parse declarations of local variables
  * e.g. let a: i32 = 5 + 7;
  */
-fn parse_assign(input: Span) -> IResult<Span, SpanAssign> {
+fn parse_local(input: Span) -> IResult<Span, SpanExpr> {
     preceded(
         tag("let"),
         map(tuple((
@@ -314,10 +357,9 @@ fn parse_assign(input: Span) -> IResult<Span, SpanAssign> {
             preceded(multispace0, parse_expr),
             preceded(multispace0, tag(";")),
         )),
-        |(ident, _, ty, _, expr, _)| (input, Assign(ident, ty, Box::new(expr))))
+            |(ident, _, ty, _, expr, _)| (input, Expr::Local(Box::new(ident), ty, Box::new(expr))))
     )(input)
 }
-
 
 
 /***************************************************************************
@@ -329,8 +371,9 @@ fn parse_assign(input: Span) -> IResult<Span, SpanAssign> {
  * Main method, program starts here.
  */
 fn main() {
+    println!("{}", 2 - 3 * 5 + 11);
     let input = "let hello30: i32 = 1 + 2;";
-    let result = parse_assign(Span::new(input));
+    let result = parse_local(Span::new(input));
     
     match result {
         Ok(n) => println!("Ok: Resulting Tree:\n    {:#?}", n),
