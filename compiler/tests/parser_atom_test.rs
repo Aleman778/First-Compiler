6f2bdbecffc297bede69455527f13c205efe9042
Alexander Mennborg <alexander.mennborg@gmail.com>
2019-09-23 21:20:18 +0200

    Updated tests of the parser

--- compiler/tests/parser_atom_test.rs
+++ compiler/tests/parser_atom_test.rs
@@ -5,72 +5,68 @@
 
 
 use compiler::{
-    ast::{Span, env::Env, atom::*},
+    ast::{Span, atom::*},
     parser::Parser,
 };
+
+
 use nom_locate::LocatedSpanEx;
 
 
-fn span<'a>(offset: usize, line: u32, frag: &'a str, env: &'a Env<'a>) -> Span<'a> {
+/**
+ * Easily define span information to test whats left after parsing.
+ */
+fn span<'a>(offset: usize, frag: &'a str) -> Span<'a> {
     LocatedSpanEx{
         offset: offset,
-        line: line,
+        line: 1,
         fragment: frag,
-        extra: env,
+        extra: (),
     }
 }
 
 
-
-
-// fn parse_ident() {
-//     assert_eq!(Ident::parse(Span::new("my_function_10")).unwrap().1,
-//                Ident{to_string: "my_function_10", span: span(0, 1, "my_function_10")});
-    
-//     assert_eq!(Ident::parse(Span::new("__WINDOWS10__")).unwrap().1,
-//                Ident{to_string: "__WINDOWS10__", span: span(0, 1, "__WINDOWS10__")});
-
-//     assert!(Ident::parse(Span::new("123Testing")).is_err());
+// #[test]
+// fn parse_fn_call() {
+    // assert_eq!(FnCall::parse(Span::new("my_function_10(100, 32)")).unwrap().1,
+               // FnCall{ident: "my_function_10", span: span(0, "my_function_10")});
 // }
 
 
 #[test]
-fn parse_val() {
-    assert_eq!(Val::parse(Span::new_extra("234", &Env::new())).unwrap().1,
-               Val::Num(LitInt{value:234, span: span(0, 1, "234", &Env::new())}));
+fn parse_ident() {
+    assert_eq!(Ident::parse(Span::new("my_function_10")).unwrap().1,
+               Ident{to_string: "my_function_10", span: span(0, "my_function_10")});
     
-    assert_eq!(Val::parse(Span::new_extra("234false", &Env::new())).unwrap(),
-               (span(3, 1, "false", &Env::new()), Val::Num(LitInt{value:234, span: span(0, 1, "234", &Env::new())})));
+    assert_eq!(Ident::parse(Span::new("__WINDOWS10__")).unwrap().1,
+               Ident{to_string: "__WINDOWS10__", span: span(0, "__WINDOWS10__")});
 
-    assert_eq!(Val::parse(Span::new_extra("true123", &Env::new())).unwrap(),
-               (span(4, 1, "123", &Env::new()), Val::Bool(LitBool{value:true, span: span(0, 1, "true", &Env::new())})));
-    
-    assert_eq!(Val::parse(Span::new_extra("false123", &Env::new())).unwrap(),
-               (span(5, 1, "123", &Env::new()), Val::Bool(LitBool{value:false, span: span(0, 1, "false", &Env::new())})));
+    assert!(Ident::parse(Span::new("123Testing")).is_err());
 }
+
 
 #[test]
 fn parse_lit_int() {
-    assert_eq!(LitInt::parse(Span::new_extra("242", &Env::new())).unwrap(),
-               (span(3, 1, "", &Env::new()), LitInt{value: 242, span: span(0, 1, "242", &Env::new())}));
+    assert_eq!(LitInt::parse(Span::new("242")).unwrap(),
+               (span(3, ""), LitInt{value: 242, span: span(0, "242")}));
     
-    assert_eq!(LitInt::parse(Span::new_extra("242abc", &Env::new())).unwrap(),
-               (span(3, 1, "abc", &Env::new()), LitInt{value: 242, span: span(0, 1, "242", &Env::new())}));
+    assert_eq!(LitInt::parse(Span::new("242abc")).unwrap(),
+               (span(3, "abc"), LitInt{value: 242, span: span(0, "242")}));
     
-    assert!(LitInt::parse(Span::new_extra("1111111111111111", &Env::new())).is_err());
+    assert!(LitInt::parse(Span::new("1111111111111111")).is_err());
 }
 
 
 #[test]
 fn parse_lit_bool() {
-    assert_eq!(LitBool::parse(Span::new_extra("true", &Env::new())).unwrap(),
-               (span(4, 1, "", &Env::new()), LitBool{value: true, span: span(0, 1, "true", &Env::new())}));
+    assert_eq!(LitBool::parse(Span::new("true")).unwrap(),
+               (span(4, ""), LitBool{value: true, span: span(0, "true")}));
     
-    assert_eq!(LitBool::parse(Span::new_extra("false", &Env::new())).unwrap(),
-               (span(5, 1, "", &Env::new()), LitBool{value: false, span: span(0, 1, "false", &Env::new())}));
+    assert_eq!(LitBool::parse(Span::new("false")).unwrap(),
+               (span(5, ""), LitBool{value: false, span: span(0, "false")}));
     
-    assert_eq!(LitBool::parse(Span::new_extra("false123", &Env::new())).unwrap(),
-               (span(5, 1, "123", &Env::new()), LitBool{value: false, span: span(0, 1, "false", &Env::new())}));
+    assert_eq!(LitBool::parse(Span::new("false123")).unwrap(),
+               (span(5, "123"), LitBool{value: false, span: span(0, "false")}));
     
-    assert!(LitBool::parse(Span::new_extra("123true", &Env::new())).is_err());
+    assert!(LitBool::parse(Span::new("123true")).is_err());
 }
---
+++ compiler/tests/parser_expr_test.rs
@@ -0,0 +1,138 @@
+
+/***************************************************************************
+ * Tests for expression parser
+ ***************************************************************************/
+
+
+/**
+ * Easily define span information to test whats left after parsing.
+ */
+fn span<'a>(offset: usize, frag: &'a str) -> Span<'a> {
+    LocatedSpanEx {
+        offset: offset,
+        line: 1,
+        fragment: frag,
+        extra: (),
+    }
+}
+
+
+#[test]
+fn parse_binop() {
+    assert_eq!(
+        BinOp::parse(Span::new("1 + 2")).unwrap().1,
+        BinOp {
+            left: Box::new(Atom::Num(LitInt{value: 1, span: span(0, "1")})),
+            op: Op::Add,
+            right: Box::new(Expr::Atom(Atom::Num(LitInt{value: 2, span: span(4, "2")}))),
+            span: span(0, "1 + 2"),
+        }
+    );
+
+
+    assert_eq!(
+        BinOp::parse(Span::new("true || false")).unwrap().1,
+        BinOp {
+            left: Box::new(Atom::Bool(LitBool{value: true, span: span(0, "true")})),
+            op: Op::Or,
+            right: Box::new(Expr::Atom(Atom::Bool(LitBool{value: false, span: span(9, "false")}))),
+            span: span(0, "true || false"),
+        }
+    );
+}
+
+
+#[test]
+fn parse_unop() {
+    assert_eq!(
+        UnOp::parse(Span::new("-42")).unwrap().1,
+        UnOp {
+            op: Op::Sub,
+            right: Box::new(Expr::Atom(Atom::Num(LitInt{value: 42, span: span(1, "42")}))),
+            span: span(0, "-42"),
+        }
+    );
+
+    assert_eq!(
+        UnOp::parse(Span::new("!false")).unwrap().1,
+        UnOp {
+            op: Op::Not,
+            right: Box::new(Expr::Atom(Atom::Bool(LitBool{value: false, span: span(1, "false")}))),
+            span: span(0, "!false"),
+        }
+    );
+}
+
+
+#[test]
+fn parse_local() {
+    assert_eq!(
+        Local::parse(Span::new("let mut a: i32 = 5;")).unwrap().1,
+        Local {
+            mutable: true,
+            ident: Ident(to_string: "a", span(8, "a")),
+            ty: ,
+            init: ,
+            span: span(0, "let mut a: i32 = 5;"),
+        }
+    );
+}
+
+
+#[test]
+fn parse_assign() {
+    assert_eq!(
+    );
+}
+
+
+#[test]
+fn parse_block() {
+
+}
+
+
+#[test]
+fn parse_if() {
+    
+}
+
+
+#[test]
+fn parse_while() {
+
+}
+
+
+#[test]
+fn parse_return() {
+
+}
+
+
+#[test]
+fn parse_break() {
+
+}
+
+
+#[test]
+fn parse_continue() {
+    
+}
+
+
+#[test]
+fn parse_atom() {
+    assert_eq!(Atom::parse(Span::new("234")).unwrap().1,
+               Atom::Num(LitInt{value:234, span: span(0, "234")}));
+    
+    assert_eq!(Atom::parse(Span::new("234false")).unwrap(),
+               (span(3, "false"), Atom::Num(LitInt{value:234, span: span(0, "234")})));
+
+    assert_eq!(Atom::parse(Span::new("true123")).unwrap(),
+               (span(4, "123"), Atom::Bool(LitBool{value:true, span: span(0, "true")})));
+    
+    assert_eq!(Atom::parse(Span::new("false123")).unwrap(),
+               (span(5, "123"), Atom::Bool(LitBool{value:false, span: span(0, "false")})));
+}
