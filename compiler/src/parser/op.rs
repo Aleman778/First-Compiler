e749e4ccb9b08fa8e67e05865503ffc0f7f24fe9
Aleman778 <alexander.mennborg@gmail.com>
2019-10-08 20:03:22 +0200

    Separated operator parser into its own file

---
+++ compiler/src/parser/op.rs
@@ -0,0 +1,50 @@
+
+/***************************************************************************
+ * Parse operators either binary or unary operators.
+ ***************************************************************************/
+
+
+
+/**
+ * Parse binary operators
+ */
+impl Parser for BinOp {
+    fn parse(input: ParseSpan) -> IResult<ParseSpan, Self> {
+        context(
+            "operator",
+            alt((
+                map(tag("+"),  |s| BinOp::Add{span: Span::new(s)}),
+                map(tag("-"),  |s| BinOp::Sub{span: Span::new(s)}),
+                map(tag("**"), |s| BinOp::Pow{span: Span::new(s)}),
+                map(tag("*"),  |s| BinOp::Mul{span: Span::new(s)}),
+                map(tag("/"),  |s| BinOp::Div{span: Span::new(s)}),
+                map(tag("%"),  |s| BinOp::Mod{span: Span::new(s)}),
+                map(tag("&&"), |s| BinOp::And{span: Span::new(s)}),
+                map(tag("||"), |s| BinOp::Or{span: Span::new(s)}),
+                map(tag("=="), |s| BinOp::Eq{span: Span::new(s)}),
+                map(tag("!="), |s| BinOp::Ne{span: Span::new(s)}),
+                map(tag("<"),  |s| BinOp::Lt{span: Span::new(s)}),
+                map(tag("<="), |s| BinOp::Le{span: Span::new(s)}),
+                map(tag(">"),  |s| BinOp::Gt{span: Span::new(s)}),
+                map(tag(">="), |s| BinOp::Ge{span: Span::new(s)}),
+            ))
+        )(input)
+    }
+}
+
+
+/**
+ * Parse unary operators
+ */
+impl Parser for UnOp {
+    fn parse(input: ParseSpan) -> IResult<ParseSpan, Self> {
+        context(
+            "operator",
+            alt((
+                map(tag("*"),  |s| UnOp::Deref{span: Span::new(s)}),
+                map(tag("!"),  |s| UnOp::Not{span: Span::new(s)}),
+                map(tag("-"),  |s| UnOp::Neg{span: Span::new(s)}),
+            ))
+        )(input)
+    }
+}
