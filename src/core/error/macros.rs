827926eff37df9eea33bf3ca24ac3af8b1be365e
Alexander Mennborg <alexander.mennborg@gmail.com>
2020-03-01 10:29:45 +0100

    Added literal expression parsing and tests (#6)
    * Almost done with the literal parser unit tests
    
    * Fixed the literal tests, and bug in lexer

--- src/core/error/macros.rs
+++ src/core/error/macros.rs
@@ -142,7 +142,7 @@ macro_rules! invalid_suffix_err {
     ($ctx:expr, $token:expr, $suffix:expr, $type:expr) => ({
         let span = Span::new($token.base + $suffix, $token.len - $suffix);
         let msg = &format!("suffixes on a raw string literal are invalid");
-        let mut err = $ctx.sess.struct_span_err(span, msg);
+        let err = $ctx.sess.struct_span_err(span, msg);
         $ctx.sess.emit(&err);
     })
 }
